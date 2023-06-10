7b9aae6da7f6f8bf95d274871a52fadcdfbbb3db
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-06-10 15:26:21 +0200

    finish key click and keyboard press logic

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,23 +1,46 @@
 import { useEffect, useState } from 'react';
 
-const Key = ({ onKeyboard, primary, secondary, keyCode }) => {
-  const [isShift, setIsShift] = useState(false); // possibly lift up
+const Key = ({ isShift, onKeyboard, primary, secondary, isDisabled }) => {
+  const [keyDown, setKeyDown] = useState(false);
 
   useEffect(() => {
-    document.addEventListener('keydown', detectKeyDown, true);
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
   }, []);
 
-  const detectKeyDown = (e) => {
-    if (e.key === secondary) {
+  const handleKeyDown = (e) => {
+    if (e.key === primary) {
+      setKeyDown(true);
       onKeyboard((prev) => prev + primary);
+    } else if (e.key === secondary) {
+      onKeyboard((prev) => prev + secondary);
     }
   };
 
-  return (
-    <div onClick={() => onKeyboard((prev) => prev + primary)} className="key">
-      <span>{isShift ? secondary : primary}</span>
-    </div>
-  );
+  const handleKeyUp = (e) => {
+    setKeyDown(false);
+  };
+
+  const handleKeyClick = (e) => {
+    if (isShift) {
+      onKeyboard((prev) => prev + primary);
+    } else {
+      onKeyboard((prev) => prev + secondary);
+    }
+  };
+
+  if (isDisabled) {
+    return <div className="key"></div>;
+  } else {
+    return (
+      <div
+        onClick={handleKeyClick}
+        className={`key ${keyDown ? 'active' : ''}`}
+      >
+        <span>{isShift ? primary : secondary}</span>
+      </div>
+    );
+  }
 };
 
 export default Key;
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,11 +1,57 @@
 import Key from './Key';
+import { useState, useEffect } from 'react';
 
 const Keyboard = ({ onKeyboard }) => {
+  const [isShift, setIsShift] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener('keydown', detectShiftDown);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('keyup', detectShiftUp, true);
+  }, []);
+
+  const detectShiftDown = (e) => {
+    if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+      setIsShift(true);
+    }
+  };
+
+  const detectShiftUp = (e) => {
+    if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+      setIsShift(false);
+    }
+  };
+
   return (
     <div className="keyboard">
-      <Key onKeyboard={onKeyboard} primary={'A'} secondary={'a'} />
-      <Key onKeyboard={onKeyboard} primary={'B'} secondary={'b'} />
-      <Key onKeyboard={onKeyboard} primary={'C'} secondary={'c'} />
+      <Key
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'A'}
+        secondary={'a'}
+        isEnabled
+      />
+      <Key
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'B'}
+        secondary={'b'}
+      />
+      <Key
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'C'}
+        secondary={'c'}
+      />
+      <Key
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'C'}
+        secondary={'c'}
+        isDisabled
+      />
     </div>
   );
 };
