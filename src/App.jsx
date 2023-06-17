8de5b0ac68bc512d8057be1afa84bb7b7e669c7f
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-06-18 00:08:49 +0200

    desktop mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Keyboard from './components/Keyboard';
+import KeyboardMobile from './components/KeyboardMobile';
 
 function App() {
   const [displayInput, setDisplayInput] = useState('');
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 780);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 780);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
+  });
 
   console.log(displayInput);
   return (
     <>
-      <Keyboard onKeyboard={setDisplayInput} />
+      {isDesktop ? (
+        <Keyboard onKeyboard={setDisplayInput} />
+      ) : (
+        <KeyboardMobile />
+      )}
     </>
   );
 }
---
+++ src/components/KeyboardMobile.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+const KeyboardMobile = ({ onKeyboard }) => {
+  const [isShift, setIsShift] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener('keydown', detectShiftDown);
+
+    return () => {
+      document.removeEventListener('keydown', detectShiftDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('keyup', detectShiftUp);
+
+    return () => {
+      document.removeEventListener('keyup', detectShiftUp);
+    };
+  }, []);
+
+  const detectShiftDown = (e) => {
+    if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+      setIsShift(true);
+    }
+  };
+
+  const detectShiftUp = (e) => {
+    if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+      setIsShift(false);
+    }
+  };
+
+  return <div className="keyboard">Hello</div>;
+};
+
+export default KeyboardMobile;
