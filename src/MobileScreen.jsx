d90d84de7605fd0997bafd6405dccbbddf38f054
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-07-23 19:33:49 +0200

    mobile background styling

--- src/MobileScreen.jsx
+++ src/MobileScreen.jsx
@@ -1,7 +1,15 @@
 const MobileScreen = ({ displayInput }) => {
   return (
     <>
-      <div className="display">{displayInput}</div>
+      <div className="phone">
+        <div className="head">
+          <span>19:28</span>
+          <div className="island"></div>
+          <span className="header-symbols"></span>
+        </div>
+        <div className="text-field">{displayInput}</div>
+        <div className="bottom-line"></div>
+      </div>
     </>
   );
 };
