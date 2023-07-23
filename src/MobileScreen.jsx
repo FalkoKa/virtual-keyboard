c8bbd280a5e495e8016657fba6580674ed5da449
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-07-23 23:55:16 +0200

    react icons for mobile display

--- src/MobileScreen.jsx
+++ src/MobileScreen.jsx
@@ -1,14 +1,26 @@
+import { BiWifi2 } from 'react-icons/bi';
+import { BsBatteryFull } from 'react-icons/bs';
+import { PiCellSignalFull } from 'react-icons/pi';
+
 const MobileScreen = ({ displayInput }) => {
   return (
     <>
       <div className="phone">
         <div className="head">
-          <span>19:28</span>
+          <span>
+            {new Date().getHours()}:{new Date().getMinutes()}
+          </span>
           <div className="island"></div>
-          <span className="header-symbols"></span>
+          <div className="header-symbols"></div>
         </div>
         <div className="text-field">{displayInput}</div>
         <div className="bottom-line"></div>
+      </div>
+      <div>
+        <BiWifi2 scale={12} />
+        <BsBatteryFull scale={12} />
+        <PiCellSignalFull scale={12} />
+        test
       </div>
     </>
   );
