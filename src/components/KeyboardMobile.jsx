8e8a648392ff21ca3c8669c445ce9b5acb3fe554
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-07-25 23:26:29 +0200

    finish 2 rows of mobile keyboard

--- src/components/KeyboardMobile.jsx
+++ src/components/KeyboardMobile.jsx
@@ -41,13 +41,133 @@ const KeyboardMobile = ({ onKeyboard }) => {
         primary={'Q'}
         secondary={'q'}
       />
-      {/* <Key
+      <Key
         mobile={true}
         isShift={isShift}
         onKeyboard={onKeyboard}
-        primary={'Q'}
-        secondary={'q'}
-      /> */}
+        primary={'W'}
+        secondary={'w'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'E'}
+        secondary={'e'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'R'}
+        secondary={'r'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'T'}
+        secondary={'t'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'Y'}
+        secondary={'y'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'U'}
+        secondary={'u'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'I'}
+        secondary={'i'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'O'}
+        secondary={'o'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'P'}
+        secondary={'p'}
+      />
+      <div className="space-3"></div>
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'A'}
+        secondary={'a'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'S'}
+        secondary={'s'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'D'}
+        secondary={'d'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'F'}
+        secondary={'f'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'G'}
+        secondary={'g'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'H'}
+        secondary={'h'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'J'}
+        secondary={'j'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'K'}
+        secondary={'k'}
+      />
+      <Key
+        mobile={true}
+        isShift={isShift}
+        onKeyboard={onKeyboard}
+        primary={'L'}
+        secondary={'l'}
+      />
     </div>
   );
 };
